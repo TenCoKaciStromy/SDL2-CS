07a8ebad4a9b9f5cb37bfd139dae097649d8e637
TcKs <muller.jakub@gmail.com>
2016-10-17 03:41:25 +0200

    SdlSafeHandles moved to sub-namespace ...Core.SafeHandles; Some new sdl function wrappers

---
+++ SDL2-PCL/Core/SafeHandles/SdlSurfaceSafeHandle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Allodium.SDL2.Native;
+
+namespace Allodium.SDL2.Core.SafeHandles {
+	public sealed class SdlSurfaceSafeHandle : SdlSafeHandle {
+		public SdlSurfaceSafeHandle(IntPtr validHandle, bool ownsHandle) : base(validHandle, ownsHandle) { }
+
+		protected override bool ReleaseHandle() {
+			SDL.SDL_FreeSurface(this.handle);
+			return true;
+		}
+	}
+}
